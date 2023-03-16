b2f92d7869c74e8e4818d0f554ce27eeddf0a318
Sarah Nsereko <sarahnsereko59@gmail.com>
2023-03-16 12:59:47 +0300

    Putting the verse in a card using material ui

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,35 +1,16 @@
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import Versecard from './Versecard'
+
 
 function Dashboard() {
-  const [verseText, setVerseText] = useState('');
+return (
+  <div>
+    <Versecard/>
+  </div>
+);
 
-  useEffect(() => {
-    axios.get('https://api.quran.com/api/v4/verses/random?language=en&words=true') //api request using the axios library
-      .then(response => {
-        const words = response.data.verse.words.sort((c, d) => c.position - d.position);
-        const verseText = words.map(word => word.translation.text).join(' ');
-
-        console.log(verseText);
-        setVerseText(verseText);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [])
-  
-  return (
-    <div>
-      <h1>Quran Verse of the Day</h1>
-      {verseText.split(' ').map((word) => (
-        <span key={word.position}>{word} </span>
-      ))}
-    </div>
-  );
 }
 
 
-
-
-export default Dashboard
+export default Dashboard;
