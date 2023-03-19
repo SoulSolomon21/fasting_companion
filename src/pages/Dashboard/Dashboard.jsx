e770648e4741ab97b1f4862a5b9040d02ba615f4
Soul_Solomon <sekamattesoul@gmail.com>
2023-03-19 16:58:33 +0300

    minor bug fix

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Versecard from './Versecard';
 import ProgressBar from './progressbar';
 import './dashboard.css'
 
 function Dashboard() {
-  const [daysCompleted, setDaysCompleted] = React.useState(0);
+  const [daysCompleted, setDaysCompleted] = useState(0);
 
   // calculate the number of days completed out of 30
-  React.useEffect(() => {
+  useEffect(() => {
     const dateStarted = new Date('2023-03-01'); // replace with actual start date
     const today = new Date();
     const daysElapsed = Math.floor((today - dateStarted) / (1000 * 60 * 60 * 24));
@@ -20,7 +20,7 @@ function Dashboard() {
       <ProgressBar />
       <h1>{daysCompleted} days completed out of 30</h1>
       <div className="verses">
-      <Versecard/>
+        <Versecard />
       </div>
     </div>
   );
