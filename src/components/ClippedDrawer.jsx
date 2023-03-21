4d76aa9d4afd3d86cc4b3a7ce689e88705ed0a76
Soul_Solomon <sekamattesoul@gmail.com>
2023-03-21 17:01:14 +0300

    feat: removed accounts page

--- src/components/ClippedDrawer.jsx
+++ src/components/ClippedDrawer.jsx
@@ -53,25 +53,19 @@ export default function ClippedDrawer() {
             <Divider />
             <ListItem disablePadding>
               <ListItemButton onClick={handleClick("calendar")}>
-              <ListItemText primary="Calendar" />              
+                <ListItemText primary="Calendar" />
               </ListItemButton>
             </ListItem>
             <Divider />
             <ListItem disablePadding>
               <ListItemButton onClick={handleClick("journal")}>
-              <ListItemText primary="Journal" />              
+                <ListItemText primary="Journal" />
               </ListItemButton>
             </ListItem>
             <Divider />
             <ListItem disablePadding>
               <ListItemButton onClick={handleClick("mealsRecommendation")}>
-              <ListItemText primary="Meals Recommendation" />              
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton onClick={handleClick("account")}>
-              <ListItemText primary="Account" />              
+                <ListItemText primary="Meals Recommendation" />
               </ListItemButton>
             </ListItem>
             <Divider />
@@ -85,3 +79,140 @@ export default function ClippedDrawer() {
     </Box>
   );
 }
+
+
+// const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+//   ({ theme, open }) => ({
+//       flexGrow: 1,
+//       padding: theme.spacing(3),
+//       transition: theme.transitions.create('margin', {
+//           easing: theme.transitions.easing.sharp,
+//           duration: theme.transitions.duration.leavingScreen,
+//       }),
+//       marginLeft: `-${drawerWidth}px`,
+//       ...(open && {
+//           transition: theme.transitions.create('margin', {
+//               easing: theme.transitions.easing.easeOut,
+//               duration: theme.transitions.duration.enteringScreen,
+//           }),
+//           marginLeft: 0,
+//       }),
+//   }),
+// );
+
+// const AppBar = styled(MuiAppBar, {
+//   shouldForwardProp: (prop) => prop !== 'open',
+// })(({ theme, open }) => ({
+//   transition: theme.transitions.create(['margin', 'width'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//   }),
+//   ...(open && {
+//       width: `calc(100% - ${drawerWidth}px)`,
+//       marginLeft: `${drawerWidth}px`,
+//       transition: theme.transitions.create(['margin', 'width'], {
+//           easing: theme.transitions.easing.easeOut,
+//           duration: theme.transitions.duration.enteringScreen,
+//       }),
+//   }),
+// }));
+
+// const DrawerHeader = styled('div')(({ theme }) => ({
+//   display: 'flex',
+//   alignItems: 'center',
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+//   justifyContent: 'flex-end',
+// }));
+
+// export default function NavBar() {
+//   const theme = useTheme();
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleDrawerOpen = () => {
+//       setOpen(true);
+//   };
+
+//   const handleDrawerClose = () => {
+//       setOpen(false);
+//   };
+
+//   return (
+//       <Box sx={{ display: 'flex' }}>
+//           <CssBaseline />
+//           <AppBar position="fixed" open={open}>
+//               <Toolbar>
+//                   <IconButton
+//                       color="inherit"
+//                       aria-label="open drawer"
+//                       onClick={handleDrawerOpen}
+//                       edge="start"
+//                       sx={{ mr: 2, ...(open && { display: 'none' }) }}
+//                   >
+//                       <MenuIcon />
+//                   </IconButton>
+//                   <Typography variant="h6" noWrap component="div">
+//                       Persistent drawer
+//                   </Typography>
+//               </Toolbar>
+//           </AppBar>
+//           <Drawer
+//               sx={{
+//                   width: drawerWidth,
+//                   flexShrink: 0,
+//                   '& .MuiDrawer-paper': {
+//                       width: drawerWidth,
+//                       boxSizing: 'border-box',
+//                   },
+//               }}
+//               variant="persistent"
+//               anchor="left"
+//               open={open}
+//           >
+//               <DrawerHeader>
+//                   <IconButton onClick={handleDrawerClose}>
+//                       {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+//                   </IconButton>
+//               </DrawerHeader>
+//               <Divider />
+//               <List>
+//                   <ListItem disablePadding>
+//                       <ListItemButton >
+//                           <ListItemText primary="Dashboard" />
+//                       </ListItemButton>
+//                   </ListItem>
+//                   <Divider />
+//                   <ListItem disablePadding>
+//                       <ListItemButton >
+//                           <ListItemText primary="Calendar" />
+//                       </ListItemButton>
+//                   </ListItem>
+//                   <Divider />
+//                   <ListItem disablePadding>
+//                       <ListItemButton >
+//                           <ListItemText primary="Journal" />
+//                       </ListItemButton>
+//                   </ListItem>
+//                   <Divider />
+//                   <ListItem disablePadding>
+//                       <ListItemButton >
+//                           <ListItemText primary="Meals Recommendation" />
+//                       </ListItemButton>
+//                   </ListItem>
+//                   <Divider />
+//                   <ListItem disablePadding>
+//                       <ListItemButton >
+//                           <ListItemText primary="Account" />
+//                       </ListItemButton>
+//                   </ListItem>
+//                   <Divider />
+//               </List>
+//           </Drawer>
+//           <Main open={open}>
+//               <DrawerHeader />
+//               <Notes />
+//           </Main>
+//       </Box>
+//   );
+// }
