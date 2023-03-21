5effd9617b0151353d8804311fcf6d7d0c4002c1
Soul_Solomon <sekamattesoul@gmail.com>
2023-03-21 18:08:47 +0300

    Renamed Drawer

--- src/components/ClippedDrawer.jsx
+++ src/components/ClippedDrawer.jsx
@@ -164,7 +164,7 @@ export default function ClippedDrawer() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Persistent drawer
+            Fasting Companion
           </Typography>
         </Toolbar>
       </AppBar>
